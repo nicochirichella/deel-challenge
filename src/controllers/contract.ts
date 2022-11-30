317cd349f40befd2562a842128521e195bd94437
nicolas chirichella <nicochirichella@gmail.com>
2022-11-29 23:02:54 -0300

    catching errors

--- src/controllers/contract.ts
+++ src/controllers/contract.ts
@@ -2,15 +2,25 @@ import { Request, Response } from "express";
 import * as contractService from "../services/contract";
 
 export const getContract = async (req: Request, res: Response) => {
-  const profile = req.app.get("profile");
-  const id = Number(req.params.id);
-  const contract = await contractService.getContractById(id, profile.id);
-  if (!contract) return res.status(404).end();
-  res.json(contract);
+    try {
+        const profile = req.app.get("profile");
+        const id = Number(req.params.id);
+        const contract = await contractService.getContractById(id, profile.id);
+        if (!contract) return res.status(404).end();
+        res.status(200).json(contract);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 };
 
 export const getContracts = async (req: Request, res: Response) => {
-  const profile = req.app.get("profile");
-  const contracts = await contractService.getContracts(profile.id);
-  res.json(contracts);
+    try {
+        const profile = req.app.get("profile");
+        const contracts = await contractService.getContracts(profile.id);
+        res.status(200).json(contracts);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 };
--- src/controllers/job.ts
+++ src/controllers/job.ts
@@ -2,16 +2,25 @@ import { Request, Response } from "express";
 import * as jobService from "../services/job";
 
 export const unpaidJobs = async (req: Request, res: Response) => {
-  const profile = req.app.get("profile");
-  const jobs = await jobService.getUnpaidJobs(profile.id);
-
-  res.json(jobs);
+    try {
+        const profile = req.app.get("profile");
+        const jobs = await jobService.getUnpaidJobs(profile.id);
+        res.status(200).json(jobs);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 };
 
 export const payJob = async (req: Request, res: Response) => {
-  const profile = req.app.get("profile");
-  const id = Number(req.params.job_id);
-  const job = await jobService.payJob(id, profile.id);
-  if (!job) return res.status(404).end();
-  res.json(job);
+    try {
+        const profile = req.app.get("profile");
+        const id = Number(req.params.job_id);
+        const job = await jobService.payJob(id, profile.id);
+        if (!job) return res.status(404).end();
+        res.status(200).json(job);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 };
